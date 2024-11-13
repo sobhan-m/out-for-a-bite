0686b642f16d5f42ed9a9bc6a5dc64ef092dd164
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2024-11-12 21:57:06 -0800

    feat: add enemy movement

---
+++ Assets/Scripts/EnemyMovementManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EnemyMovementManager : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private EnemyTarget enemyTarget;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyTarget = FindClosestEnemyTarget();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (enemyTarget == null)
+        {
+            throw new MissingReferenceException("Unable to find an enemy in the scene.");
+        }
+
+
+        Vector3 vectorToTarget = enemyTarget.transform.position - transform.position;
+        Vector3 velocity = vectorToTarget.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(transform.position + velocity);
+    }
+
+    private EnemyTarget FindClosestEnemyTarget()
+    {
+        EnemyTarget[] targets = FindObjectsOfType<EnemyTarget>();
+        EnemyTarget closestEnemyTarget = null;
+        float closestDistance = float.MaxValue;
+        foreach (EnemyTarget target in targets)
+        {
+            float distance = (target.gameObject.transform.position - transform.position).magnitude;
+            if (distance < closestDistance)
+            {
+                closestDistance = distance;
+                closestEnemyTarget = target;
+            }
+        }
+        return closestEnemyTarget;
+    }
+}
--- Assets/Scripts/Fangs/FangCollector.cs
+++ Assets/Scripts/Fangs/FangCollector.cs
@@ -2,15 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Collision2D))]
 public class FangCollector : MonoBehaviour
 {
-    private Collision2D collision2D;
-    void Start()
-    {
-        collision2D = GetComponent<Collision2D>();
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Fang fang = collision.GetComponent<Fang>();
