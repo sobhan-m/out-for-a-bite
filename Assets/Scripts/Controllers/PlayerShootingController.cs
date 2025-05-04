9c7c919105bf12c791213e39f5d74f9597490114
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2025-05-03 23:45:59 -0700

    feat(Reload): implement reloading mechanic

--- Assets/Scripts/Controllers/PlayerShootingController.cs
+++ Assets/Scripts/Controllers/PlayerShootingController.cs
@@ -8,36 +8,41 @@ public class PlayerShootingController : MonoBehaviour
 {
     [SerializeField] public GameObject bulletPrefab;
     [SerializeField] public float secondsBetweenShots;
+    [SerializeField] public int magSize;
+    [SerializeField] public float secondsBeforeReload;
     private Meter shootingCooldown;
+    private Meter mag;
     private Camera cam;
     private InputActionAsset actions;
     private InputAction shootAction;
+    private Meter reloadCooldown;
 
 
     private void Awake()
     {
         actions = FindObjectOfType<InputActionContainingSystem>().actions;
-        Debug.Log(actions);
         shootAction = actions.FindActionMap("Player").FindAction("Shoot");
+        shootAction.performed += OnShoot;
     }
 
     void Start()
     {
         shootingCooldown = new Meter(0, secondsBetweenShots);
+        mag = new Meter(0, magSize, magSize);
+        reloadCooldown = new Meter(0, secondsBeforeReload);
         cam = Camera.main;
     }
 
     void Update()
     {
-        shootingCooldown.EmptyMeter(Time.deltaTime);
-        if (shootAction.IsPressed() && shootingCooldown.IsEmpty())
-        {
-            Shoot();
-        }
+        CooldownShooting();
+        CooldownReload();
     }
 
     private void Shoot()
     {
+        mag.EmptyMeter(1);
+
         Vector3 mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = gameObject.transform.position.z;
         Vector3 currentPosition = gameObject.transform.position;
@@ -50,6 +55,32 @@ public class PlayerShootingController : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().velocity = bulletVelocity;
 
         shootingCooldown.FillMeter();
+    }
+
+    private void OnShoot(InputAction.CallbackContext context)
+    {
+        if (shootingCooldown.IsEmpty() && !mag.IsEmpty())
+        {
+            Shoot();
+        }
+    }
+
+    private void CooldownShooting()
+    {
+        shootingCooldown.EmptyMeter(Time.deltaTime);
+    }
+
+    private void CooldownReload()
+    {
+        if (mag.IsEmpty())
+        {
+            reloadCooldown.EmptyMeter(Time.deltaTime);
+            if (reloadCooldown.IsEmpty())
+            {
+                reloadCooldown.FillMeter();
+                mag.FillMeter();
+            }
+        }
     }
 
     private void OnEnable()
