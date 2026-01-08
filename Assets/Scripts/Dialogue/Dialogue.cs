de5dca684b774ffc026600572fc9cc3e04771a22
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2026-01-07 23:33:13 -0800

    feat: add dialogue to game

--- Assets/Scripts/Dialogue/Dialogue.cs
+++ Assets/Scripts/Dialogue/Dialogue.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Dialogue", menuName = "Dialogue/Dialogue", order = 0)]
 public class Dialogue : ScriptableObject {
     public string speaker;
+    [TextArea]
     public string dialogueText;
     public GameObject sceneGameObject;   
 }
