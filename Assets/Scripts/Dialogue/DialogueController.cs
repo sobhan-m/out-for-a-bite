b87080e0e6c04c6ea035a3f36d9bff194812475f
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2026-03-14 01:56:29 -0700

    Revert "feat: add null guard"
    This reverts commit 3873e4993614faf963faa693e857e83a6fc89f6f.

--- Assets/Scripts/Dialogue/DialogueController.cs
+++ Assets/Scripts/Dialogue/DialogueController.cs
@@ -86,10 +86,6 @@ public class DialogueController : MonoBehaviour
 
     private void UpdateCharacterImage(Sprite sprite, DialoguePosition positionBeingFilled, DialoguePosition speakerPosition)
     {
-        if (sprite == null)
-        {
-            return;
-        }
         int positionBeingFilledIndex = (int) positionBeingFilled;
         if (sprite != null)
         {
