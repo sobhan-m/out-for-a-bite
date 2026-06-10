8870525acc16c5928372f0ddb3f5c4467cd8810a
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2026-06-09 22:07:00 -0700

    feat: add simple garlic lover mode

--- Assets/Scripts/GarlicReserve.cs
+++ Assets/Scripts/GarlicReserve.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class GarlicReserve : MonoBehaviour
 {
-    [SerializeField] public int garlicCount = 12;
+    [SerializeField] public int garlicCount = 3;
 
-    public bool HasGarlic()
+	void Start()
+	{
+        SettingSystem settingSystem = FindObjectOfType<SettingSystem>();
+		if (settingSystem != null && settingSystem.isGarlicLoverMode)
+        {
+            garlicCount = SettingSystem.GARLIC_LOVER_MODE_COUNT;
+        }
+	}
+
+	public bool HasGarlic()
     {
         return garlicCount > 0;
     }
---
+++ Assets/Scripts/Systems/SettingSystem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingSystem : MonoBehaviour
+{
+    public const int GARLIC_LOVER_MODE_COUNT = 100;
+
+    [SerializeField]
+    public bool isGarlicLoverMode = false;
+}
