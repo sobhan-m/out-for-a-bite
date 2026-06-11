bea2f90be31ef1caa465ac9143949ea9c1c807ab
Sobhan <72103946+sobhan-m@users.noreply.github.com>
2026-06-11 06:12:30 -0700

    feat: add Save file

---
+++ Assets/Scripts/Systems/Save.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Save
+{
+    public const string SCENE_NAME = "SCENE_NAME";
+    public const string IS_GARLIC_LOVER_MODE = "IS_GARLIC_LOVER_MODE";
+    public string sceneName;
+    public bool isGarlicLoverMode;
+}
